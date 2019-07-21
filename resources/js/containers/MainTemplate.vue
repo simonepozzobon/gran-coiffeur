dd7f5565f3d928ef2caa0dacc87505332dd19545
Angus Simons <info@simonepozzobon.com>
2019-07-22 00:09:06 +0200

    Initial commit

---
+++ resources/js/containers/MainTemplate.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    MainTemplate
+</div>
+</template>
+
+<script>
+export default {
+    name: 'MainTemplate',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/shared';
+</style>
